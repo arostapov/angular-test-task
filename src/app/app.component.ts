d43a591e63a6a6ebf97091112b65c3927eb253b8
arostapov <arostapov@gmail.com>
2024-10-06 19:33:29 +0200

    chore(app): Remove useless files. Add app routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
-import {Component} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {CheckUserResponseData} from "./shared/interface/responses";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet />',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-
-  constructor(private http: HttpClient) {
-  }
-
-
-  // just an example, you are free to move it anywhere
-  checkUser(username: string): Observable<CheckUserResponseData> {
-    return this.http.post<CheckUserResponseData>('/api/checkUsername1', {username});
-  }
-}
+export class AppComponent {}
